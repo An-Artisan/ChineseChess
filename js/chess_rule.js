3a295528b92e8a6adbd343e3c83c3a5f007adc5a
DESKTOP-NBP4V3D\不敢为天下 <1090035743@qq.com>
2017-05-20 01:06:40 +0800

    五次提交： 1：兵的规则BUG的修改，如果不超过棋盘内，才设置为兵的活跃棋位 2：全局JSON的BUG修改，忘记重置全局JSON为空 3：炮的上下左右可走棋位建立 4：炮的所有规则完全建立 5：完善了可走棋位的点击事件冗余代码。把可走棋位的点击事件写在一个函数内。兵和炮的可走棋位直接调用该函数，共享吃棋子的规则。

--- js/chess_rule.js
+++ js/chess_rule.js
@@ -2,18 +2,18 @@ var red_status = 0;
 var black_status = 0;
 var global_src = null;
 var global_target = {};
-// var a = $(".chess_board img[position='102']").attr('src');
-// console.log(a);
 function rule(e){
 	// 获取棋子的类型
 	var chessman = $(e).attr('chessman');
 	switch(chessman)
 	{
 	case 'r_b':
+	  // 调用兵的规则
 	  soldier(e);
 	  break;
 	case 'r_p':
-	  // soldier(e);
+	  // 调用炮的规则
+	  cannon(e);
 	  break;
 	case 'r_j':
 	  // soldier(e);
@@ -32,6 +32,194 @@ function rule(e){
 	  break;
 	default:
 	}
+	
+}
+function set_chessman_active(){
+	// 循环全局json
+	$.each(global_target,function(name,value) {
+		// 获取当前位置的name
+		var attr_name = $(".chess_board img[position='"+name+"']").attr('name');
+		// 获取当前位置的chessman
+		var attr_chessman = $(".chess_board img[position='"+name+"']").attr('chessman');
+		// 如果attr_name为null表示为空棋位
+		if(attr_name == 'null'){
+			// 设置可走的位置为活跃棋位
+			$(".chess_board img[position='"+name+"']").attr('src','./images_chess/XQSTUDIO/OOS.GIF');
+			// 设置棋位的chessman_position值为active
+			$(".chess_board img[position='"+name+"']").attr('chessman_position','active');
+		}
+		// 如果可走棋位不为我方棋子就添加为活跃棋位
+		else if(attr_chessman.substring(0,1)!='r'){
+			// 获取活跃棋位的src
+			var s = $(".chess_board img[position='"+name+"']").attr('src');
+			// 拼接字符串
+			s = s.substring(0,s.length-4) + 'S.GIF'
+			// 设置棋位为可走棋位
+			$(".chess_board img[position='"+name+"']").attr('src',s);
+			// 设置chessman_position为active，用于点击判断
+			$(".chess_board img[position='"+name+"']").attr('chessman_position','active');
+			// 设置name为chessman_active，用于区分对方棋子不可点击
+			$(".chess_board img[position='"+name+"']").attr('name','chessman_active');
+		}
+		
+	});
+}
+// 炮的规则
+function cannon(e){
+	// 获取当前棋子的位置
+	var position = parseInt($(e).attr('position'));
+	// 获取个位
+	var units = position - ((Math.floor(position/10))*10);
+	// 把当前棋子位置赋值给i，用于上下棋位的判断
+	var i = position;
+	// 把当前棋子的位置个位数赋值给j，用于左右棋位的判断
+	var j = units;
+	// 直接判断当前棋位的上一个棋位
+	i += 10;
+	// 循环判断，如果这个棋位小于110 代表在棋盘内。并且name值为null，代表是空棋位
+	while(i <110 && $(".chess_board img[position='"+i+"']").attr('name') == 'null'){
+			// 添加至全局json
+			global_target[i] = $(".chess_board img[position='"+i+"']").attr('src');
+			// 自增10，判断下一个棋位
+			i += 10;
+	}
+	// i自增10 表示在上一次棋位的name值不为null的基础上在往前一个的棋位
+	i += 10;
+	// 如果小于110，代表在棋盘内，并且它的name值不为空，代表炮吃棋子的规则
+	while(i < 110){
+		if($(".chess_board img[position='"+i+"']").attr('name') != 'null'){
+			// 判断是不是我方棋子，不是我方棋子才添加至全局json
+			if($(".chess_board img[position='"+i+"']").attr('chessman').substring(0,1)!='r'){
+				// 添加至全局json
+				global_target[i] = $(".chess_board img[position='"+i+"']").attr('src');
+				// 跳出循环
+				break;
+			}
+			// 是我方棋子也跳出循环，因为炮的规则不允许吃我方棋子
+			else{
+				// 跳出循环
+				break;
+			}
+		}
+		// 自增10，判断下一个棋位
+		i += 10;
+	}
+	// 重新赋值之前的位置
+	i = position;
+	// i++ 表示当前棋子右边的第一个棋位
+	i ++;
+	// j++ 表示当前棋子左边第一个棋位的个位
+	j ++;
+	// 如果当前的个位小于10，并且当前棋子的值为空棋位
+	while(j < 10 && $(".chess_board img[position='"+i+"']").attr('name') == 'null'){
+			// 添加至全局json
+			global_target[i] = $(".chess_board img[position='"+i+"']").attr('src');
+			// i++ 判断下一个棋位
+			i ++;
+			// j++ 判断下一个棋位的个位
+			j ++;
+	}
+	// i++ 表示在上一次棋位的name值不为null的基础上在往右一个的棋位
+	i ++;
+	// j++ 表示在上一次棋位个位
+	j ++;
+	// 如果j小于10，代表在棋盘内，并且它的name值不为空，代表炮吃棋子的规则
+	while(j < 10){
+		if($(".chess_board img[position='"+i+"']").attr('name') != 'null'){
+			// 判断是不是我方棋子，不是我方棋子才添加至全局json
+			if($(".chess_board img[position='"+i+"']").attr('chessman').substring(0,1)!='r'){
+				// 添加至全局json
+				global_target[i] = $(".chess_board img[position='"+i+"']").attr('src');
+				// 跳出循环
+				break;
+			}
+			// 是我方棋子也跳出循环，因为炮的规则不允许吃我方棋子
+			else{
+				// 跳出循环
+				break;
+			}
+		}
+		// 自增1，判断下一个棋位
+		i ++;
+		// 自增1，判断下一个棋位
+		j ++;
+	}
+	// 重新赋值个位
+	j = units;
+	// 重新赋值位置
+	i = position;
+	// i-10 判断当前棋子后一位的棋子
+	i -= 10;
+	// 当前位置大于10，表示在棋盘内，并且name值等于null，是空棋位才添加到全局json
+	while(i > 10 && $(".chess_board img[position='"+i+"']").attr('name') == 'null'){
+			// 添加至全局json
+			global_target[i] = $(".chess_board img[position='"+i+"']").attr('src');
+			// i-10，判断下一个棋位
+			i -= 10;
+	}
+	// i-10 表示在上一次棋位的name值不为null的基础上在往后一个的棋位
+	i -= 10;
+	// 当前位置大于10，代表在棋盘内，并且它的name值不为空，代表炮吃棋子的规则
+	while(i > 10){
+		if($(".chess_board img[position='"+i+"']").attr('name') != 'null'){
+			// 判断是否是我方棋子，不是我方棋子，代表可以吃，添加至全局json
+			if($(".chess_board img[position='"+i+"']").attr('chessman').substring(0,1)!='r'){
+				// 添加至全局json
+				global_target[i] = $(".chess_board img[position='"+i+"']").attr('src');
+				// 跳出循环
+				break;
+			}
+			// 是我方棋子也跳出循环，因为不能吃自家棋子
+			else{
+				// 跳出循环
+				break;
+			}
+		}
+		// i-10 判断下一个棋位
+		i -= 10;
+	}
+	// 重新赋值棋子的位置
+	i = position;
+	// i-- 直接判断当前棋位的左一个棋位
+	i --;
+	// j-- 表示往左一个棋位的个位
+	j --;
+	// 个位小于0，代表在棋盘内，并且name值为空，表示是空棋位，可以添加到全局json
+	while(j > 0 && $(".chess_board img[position='"+i+"']").attr('name') == 'null'){
+			// 添加到全局json
+			global_target[i] = $(".chess_board img[position='"+i+"']").attr('src');
+			// 自减1，判断下一个棋位
+			i --;
+			// 自减1，判断下一个棋位
+			j --;
+	}
+	// i-- 表示在上一次棋位的name值不为null的基础上在往左一个的棋位
+	i --;
+	// j-- 表示往左一个棋位的个位
+	j --;
+	// 个位小于0，代表在棋盘内，代表在棋盘内，并且它的name值不为空，代表炮吃棋子的规则
+	while(j > 0){
+		if($(".chess_board img[position='"+i+"']").attr('name') != 'null'){
+			// 判断是否是我方棋子，不是我方棋子，代表可以吃，添加至全局json
+			if($(".chess_board img[position='"+i+"']").attr('chessman').substring(0,1)!='r'){
+				// 添加至全局json
+				global_target[i] = $(".chess_board img[position='"+i+"']").attr('src');
+				// 跳出循环
+				break;
+			}
+			// 是我方棋子也跳出循环，因为不能吃自家棋子。
+			else{
+				// 跳出循环
+				break;
+			}
+		}
+		// 判断下一个棋位
+		i --;
+		// 判断下一个棋位
+		j --;
+	}
+	// 设置棋位为活跃可吃棋位
+	set_chessman_active();
 	
 }
 function soldier(e){
@@ -80,7 +268,9 @@ function soldier(e){
 				var up = $(".chess_board img[position='"+(position+10)+"']").attr('src');
 				var left = $(".chess_board img[position='"+(position-1)+"']").attr('src');
 				// 添加到全局json
-				global_target[position+10] = up;
+				if(position < 100){
+					global_target[position+10] = up;
+				}
 				global_target[position-1] = left;
 				// 跳出switch
 			    break;
@@ -90,7 +280,9 @@ function soldier(e){
 				var up = $(".chess_board img[position='"+(position+10)+"']").attr('src');
 				var right = $(".chess_board img[position='"+(position+1)+"']").attr('src');
 				// 添加到全局json
-				global_target[position+10] = up;
+				if(position < 100){
+					global_target[position+10] = up;
+				}
 				global_target[position+1] = right;
 				// 跳出switch
 				break;
@@ -101,38 +293,14 @@ function soldier(e){
 				var right = $(".chess_board img[position='"+(position+1)+"']").attr('src');
 				var left = $(".chess_board img[position='"+(position-1)+"']").attr('src');
 				// 添加到全局json
-				global_target[position+10] = up;
+				if(position < 100){
+					global_target[position+10] = up;
+				}
 				global_target[position-1] = left;
 				global_target[position+1] = right;
 		}
-		// 循环全局json
-		$.each(global_target,function(name,value) {
-			// 获取当前位置的name
-			var attr_name = $(".chess_board img[position='"+name+"']").attr('name');
-			// 获取当前位置的chessman
-			var attr_chessman = $(".chess_board img[position='"+name+"']").attr('chessman');
-			// 如果attr_name为null表示为空棋位
-			if(attr_name == 'null'){
-				// 设置可走的位置为活跃棋位
-				$(".chess_board img[position='"+name+"']").attr('src','./images_chess/XQSTUDIO/OOS.GIF');
-				// 设置棋位的chessman_position值为active
-				$(".chess_board img[position='"+name+"']").attr('chessman_position','active');
-			}
-			// 如果可走棋位不为我方棋子就添加为活跃棋位
-			else if(attr_chessman.substring(0,1)!='r'){
-				// 获取活跃棋位的src
-				var s = $(".chess_board img[position='"+name+"']").attr('src');
-				// 拼接字符串
-				s = s.substring(0,s.length-4) + 'S.GIF'
-				// 设置棋位为可走棋位
-				$(".chess_board img[position='"+name+"']").attr('src',s);
-				// 设置chessman_position为active，用于点击判断
-				$(".chess_board img[position='"+name+"']").attr('chessman_position','active');
-				// 设置name为chessman_active，用于区分对方棋子不可点击
-				$(".chess_board img[position='"+name+"']").attr('name','chessman_active');
-			}
-			
-		});
+		// 循环全局json的活跃可吃棋位
+		set_chessman_active();
 		
 	}
 
@@ -180,7 +348,7 @@ $('.chess_board').on('click',"img[chessman_position='active']",function(){
 		});
 		
 	});
-
+// 棋子的点击事件
 $('.chess_board').on('click',"img[name='chessman']",function(){
 		var src;
 		// 设置棋子状态为非活跃
@@ -212,6 +380,8 @@ $('.chess_board').on('click',"img[name='chessman']",function(){
 				$(".chess_board img[position='"+name+"']").removeAttr('chessman_position');
 
 			});
+			// 清空全局目标棋位
+			global_target = {};
 
 		}
 		// 设置棋子状态为活跃
@@ -235,4 +405,4 @@ $('.chess_board').on('click',"img[name='chessman']",function(){
 				rule(this);
 			}
 		}
-	});
+});
